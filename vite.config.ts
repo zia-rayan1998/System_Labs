4b606b9fcd5648aa3df39a8c8e720c8dbef63820
Mohammed Zia Uddin <ziauddinrayan97@gmail.com>
2025-12-31 19:57:31 +0530

    added vite configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+  import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
